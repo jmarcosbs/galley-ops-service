5a437b8d68718e2e1bb7823977fa85083dda8b29
João Marcos <jmarcosmmn@gmail.com>
2025-12-14 01:32:50 +0000

    fixing production

--- core/settings.py
+++ core/settings.py
@@ -159,14 +159,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-USE_X_FORWARDED_HOST = True
-
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
